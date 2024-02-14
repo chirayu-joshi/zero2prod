05b476e7ef2ee6f654ee18d1676a827be1e78baf
chirayu-joshi <chirayujoshi1189@gmail.com>
2024-02-15 02:14:16 +0530

    Completed /subscription API

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::net::TcpListener;
 
-use zero2prod::run;
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = 8000;
-    let listener = TcpListener::bind(format!("127.0.0.1:{}", port))
-        .unwrap_or_else(|_| panic!("Failed to bind port: {}", port));
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
 
-    run(listener)?.await
+    let listener = TcpListener::bind(address)?;
+
+    run(listener, connection_pool)?.await
 }
